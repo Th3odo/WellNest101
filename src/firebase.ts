56513286e7ee30679576b3a691d344e59cc9b543
Theodore Hororo <142614907+Th3odo@users.noreply.github.com>
2025-04-12 07:32:50 +0200

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
+  apiKey: "AIzaSyCI8WszgNKfANAliPZHtpMsKi1KxgKPFns",
+  authDomain: "wellnest-d0723.firebaseapp.com",
   projectId: "wellnest-d0723",
   storageBucket: "wellnest-d0723.firebasestorage.app",
   messagingSenderId: "977428306043",
   appId: "1:977428306043:web:88b8e5b45986f5dec4eade"
-};;
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
